49f7998d180d9ec9cc2fb4c4525016860fc6161e
Garber1329 <91221454+Garber1329@users.noreply.github.com>
2021-11-04 21:43:27 +0200

    Delete lab_1/Task1 directory

--- lab_1/Task1/FieldsAreIncorrect.java
+++
@@ -1,7 +0,0 @@
-package com.company.Task1;
-
-public class FieldsAreIncorrect extends Exception{
-    public FieldsAreIncorrect(String message) {
-        super(message);
-    }
-}
--- lab_1/Task1/httpCode.java
+++
@@ -1,7 +0,0 @@
-package com.company.Task1;
-
-public enum httpCode {
-    HTTP_CODE_200,
-    HTTP_CODE_400,
-    HTTP_CODE_500;
-}
